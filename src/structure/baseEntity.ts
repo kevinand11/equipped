624d134f87b3acbec686cde1ad0c02d6b34dc73b
kevinand11 <kevinfizu@gmail.com>
2023-04-07 14:22:39 +0100

    feat: add list to ignore when converting baseEntity to json

--- src/structure/baseEntity.ts
+++ src/structure/baseEntity.ts
@@ -1,7 +1,15 @@
 import { Random } from '../utils/utils'
 
+const deleteKeyFromObject = (obj: Record<string, any>, keys: string[]) => {
+	if (obj === undefined || obj === null) return
+	if (keys.length === 1) return delete obj[keys[0]]
+	const key = keys.pop() ?? ''
+	return deleteKeyFromObject(obj[key], keys)
+}
+
 export class BaseEntity {
 	public hash: string
+	public ignoreInJSON = [] as string[]
 
 	constructor () {
 		this.hash = Random.string()
@@ -9,14 +17,8 @@ export class BaseEntity {
 
 	toJSON () {
 		const json = Object.assign({}, this) as Record<string, any>
-		const proto = Object.getPrototypeOf(this)
-		Object.getOwnPropertyNames(proto)
-			.filter((k) => k !== 'constructor')
-			.forEach((key) => {
-				const value = this[key as keyof BaseEntity]
-				// @ts-ignore
-				json[key] = value?.toJSON?.() ?? value
-			})
+		this.ignoreInJSON.forEach((k) => deleteKeyFromObject(json, k.split('.').reverse()))
+		delete json.ignoreInJSON
 		return json
 	}
 }
