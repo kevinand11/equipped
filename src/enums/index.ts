d7fbd0f7d66b07ff624538ba43f004c864971253
kevinand11 <kevinfizu@gmail.com>
2025-05-14 23:57:26 +0100

    refactor: wrap all enums in an object cos of reference failure after build

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -19,21 +19,17 @@ type Keys = keyof {
 	CronTypes: ICronTypes
 }
 
-export let AuthRole: IAuthRole = DefaultAuthRole as any
-export let AuthTypes: IAuthTypes = DefaultAuthTypes as any
-export let EmailsList: IEmailsList = DefaultEmailsList as any
-export let EventTypes: IEventTypes = DefaultEventTypes as any
-export let DelayedJobs: IDelayedJobs = DefaultDelayedJobs as any
-export let CronLikeJobs: ICronLikeJobs = DefaultCronLikeJobs as any
-export let CronTypes: ICronTypes = DefaultCronTypes as any
+export class Enums {
+	static AuthRole: IAuthRole = DefaultAuthRole as any
+	static AuthTypes: IAuthTypes = DefaultAuthTypes as any
+	static EmailsList: IEmailsList = DefaultEmailsList as any
+	static EventTypes: IEventTypes = DefaultEventTypes as any
+	static DelayedJobs: IDelayedJobs = DefaultDelayedJobs as any
+	static CronLikeJobs: ICronLikeJobs = DefaultCronLikeJobs as any
+	static CronTypes: ICronTypes = DefaultCronTypes as any
 
-export const makeEnum = <T extends Record<string, any>>(key: Keys, obj: T): Readonly<T> => {
-	if (key === 'AuthRole') return (AuthRole = { ...AuthRole, ...obj })
-	if (key === 'AuthTypes') return (AuthTypes = { ...AuthTypes, ...obj })
-	if (key === 'EmailsList') return (EmailsList = { ...EmailsList, ...obj })
-	if (key === 'EventTypes') return (EventTypes = { ...EventTypes, ...obj })
-	if (key === 'DelayedJobs') return (DelayedJobs = { ...DelayedJobs, ...obj })
-	if (key === 'CronLikeJobs') return (CronLikeJobs = { ...CronLikeJobs, ...obj })
-	if (key === 'CronTypes') return (CronTypes = { ...CronTypes, ...obj })
-	return obj
+	static make<T extends Record<string, any>> (key: Keys, obj: T): Readonly<T> {
+		if (Enums[key] && typeof Enums[key] === 'object') Enums[key] = Object.freeze({ ...Enums[key], ...obj }) as any
+		return obj
+	}
 }
