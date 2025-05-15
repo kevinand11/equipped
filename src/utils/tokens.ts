16a1beca0970735648722b711c1396768415a52f
kevinand11 <kevinfizu@gmail.com>
2025-05-15 09:43:44 +0100

    refactor: abstract tokens utility

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -6,79 +6,113 @@ import { Instance } from '../instance'
 import { StatusCodes } from '../server'
 import type { AuthUser, RefreshUser } from './authUser'
 
-const getAccessTokenKey = (userId: string) => `${userId}-access-token`
-const getRefreshTokenKey = (userId: string) => `${userId}-refresh-token`
 
-export const makeAccessToken = async (payload: AuthUser) => {
-	const token = jwt.sign(payload, Instance.get().settings.requestsAuth.accessToken.key, { expiresIn: Instance.get().settings.requestsAuth.accessToken.ttl })
-	await Instance.get().cache.set(getAccessTokenKey(payload.id), token, Instance.get().settings.requestsAuth.accessToken.ttl)
-	return token
-}
-export const makeRefreshToken = async (payload: RefreshUser) => {
-	const token = jwt.sign(payload, Instance.get().settings.requestsAuth.refreshToken.key, { expiresIn: Instance.get().settings.requestsAuth.refreshToken.ttl })
-	await Instance.get().cache.set(getRefreshTokenKey(payload.id), token, Instance.get().settings.requestsAuth.refreshToken.ttl)
-	return token
-}
+export abstract class BaseTokensUtility {
+	abstract createAccessToken(payload: AuthUser) :Promise<string>
+	abstract createRefreshToken(payload: RefreshUser) :Promise<string>
+	abstract verifyAccessToken(token: string) :Promise<AuthUser>
+	abstract verifyRefreshToken (token: string): Promise<RefreshUser>
+	abstract retrieveAccessTokenFor(userId: string) :Promise<string | null>
+	abstract retrieveRefreshTokenFor(userId: string) :Promise<string | null>
+	abstract deleteAccessTokenFor(userId: string) :Promise<void>
+	abstract deleteRefreshTokenFor(userId: string) :Promise<void>
 
-export const verifyAccessToken = async (token: string) => {
-	try {
-		const user = jwt.verify(token, Instance.get().settings.requestsAuth.accessToken.key) as AuthUser
-		if (!user) throw new NotAuthenticatedError()
-		const cachedToken = await getCachedAccessToken(user.id)
-		// Cached access token was deleted, e.g. by user roles being modified, so token needs to be treated as expired
-		if (token && token !== cachedToken) throw new AccessTokenExpired()
-		return user
-	} catch (err) {
-		if (err instanceof AccessTokenExpired) throw err
-		if (err instanceof jwt.TokenExpiredError) throw new AccessTokenExpired()
-		else throw new NotAuthenticatedError()
+	async exchangeTokens (tokens: {
+		accessToken: string
+		refreshToken: string
+	}, getPayload: (id: string) => Promise<{ access: AuthUser; refresh: RefreshUser }>): Promise<{
+		accessToken: string
+		refreshToken: string
+	}> {
+		const authUser = await this.verifyAccessToken(tokens.accessToken).catch((err) => {
+			const error = err as CustomError
+			if (error.statusCode === StatusCodes.AccessTokenExpired) return null
+			else throw err
+		})
+		if (authUser) return tokens
+
+		const refreshUser = await this.verifyRefreshToken(tokens.refreshToken)
+		// const cachedRefreshToken = await getCachedRefreshToken(refreshUser.id)
+
+		// If no cached value, means someone used your old token for a second time, so current one got deleted from cache
+		// if (!cachedRefreshToken) throw new NotAuthenticatedError()
+
+		// If cached value is not equal, means someone is trying to use an old token for a second time
+		// if (refreshToken !== cachedRefreshToken) {
+		// await deleteCachedAccessToken(refreshUser.id)
+		// await deleteCachedRefreshToken(refreshUser.id)
+		// throw new RefreshTokenMisusedError()
+		// }
+
+		const payloads = await getPayload(refreshUser.id)
+		return {
+			accessToken: await this.createAccessToken(payloads.access),
+			refreshToken: await this.createRefreshToken(payloads.refresh),
+		}
 	}
 }
 
-export const verifyRefreshToken = async (token: string) => {
-	try {
-		const user = jwt.verify(token, Instance.get().settings.requestsAuth.refreshToken.key) as RefreshUser
-		if (!user) throw new NotAuthenticatedError()
-		return user
-	} catch {
-		throw new NotAuthenticatedError()
+export class CacheTokensUtility extends BaseTokensUtility {
+	#getAccessTokenKey: (userId: string) => string
+	#getRefreshTokenKey: (userId: string) => string
+	constructor (options: { accessTokenPrefix?: string, refreshTokenPrefix?: string } = {}) {
+		super()
+		const accessPrefix = options?.accessTokenPrefix ?? 'tokens.access.'
+		const refreshPrefix = options?.accessTokenPrefix ?? 'tokens.refresh.'
+		this.#getAccessTokenKey = (userId: string) => `${accessPrefix}${userId}`
+		this.#getRefreshTokenKey = (userId: string) => `${refreshPrefix}${userId}`
 	}
-}
 
-export const getCachedAccessToken = async (userId: string) => Instance.get().cache.get(getAccessTokenKey(userId))
-export const getCachedRefreshToken = async (userId: string) => Instance.get().cache.get(getRefreshTokenKey(userId))
-export const deleteCachedAccessToken = async (userId: string) => Instance.get().cache.delete(getAccessTokenKey(userId))
-export const deleteCachedRefreshToken = async (userId: string) => Instance.get().cache.delete(getRefreshTokenKey(userId))
+	async createAccessToken (payload: AuthUser) {
+		const token = jwt.sign(payload, Instance.get().settings.requestsAuth.accessToken.key, { expiresIn: Instance.get().settings.requestsAuth.accessToken.ttl })
+		await Instance.get().cache.set(this.#getAccessTokenKey(payload.id), token, Instance.get().settings.requestsAuth.accessToken.ttl)
+		return token
+	}
 
-type Tokens = {
-	accessToken: string
-	refreshToken: string
-}
+	async createRefreshToken (payload: RefreshUser) {
+		const token = jwt.sign(payload, Instance.get().settings.requestsAuth.refreshToken.key, { expiresIn: Instance.get().settings.requestsAuth.refreshToken.ttl })
+		await Instance.get().cache.set(this.#getRefreshTokenKey(payload.id), token, Instance.get().settings.requestsAuth.refreshToken.ttl)
+		return token
+	}
 
-export const exchangeOldForNewTokens = async (
-	{ accessToken, refreshToken }: Tokens,
-	makeTokens: (id: string) => Promise<Tokens>,
-): Promise<Tokens> => {
-	const authUser = await verifyAccessToken(accessToken).catch((err) => {
-		const error = err as CustomError
-		if (error.statusCode === StatusCodes.AccessTokenExpired) return null
-		else throw err
-	})
-	// If auth token is not expired, get the user id from it
-	if (authUser) return await makeTokens(authUser.id)
+	async verifyAccessToken (token: string) {
+		try {
+			const user = jwt.verify(token, Instance.get().settings.requestsAuth.accessToken.key) as AuthUser
+			if (!user) throw new NotAuthenticatedError()
+			const cachedToken = await this.retrieveAccessTokenFor(user.id)
+			// Cached access token was deleted, e.g. by user roles being modified, so token needs to be treated as expired
+			if (token && token !== cachedToken) throw new AccessTokenExpired()
+			return user
+		} catch (err) {
+			if (err instanceof AccessTokenExpired) throw err
+			if (err instanceof jwt.TokenExpiredError) throw new AccessTokenExpired()
+			else throw new NotAuthenticatedError()
+		}
+	}
 
-	const refreshUser = await verifyRefreshToken(refreshToken)
-	// const cachedRefreshToken = await getCachedRefreshToken(refreshUser.id)
+	async verifyRefreshToken (token: string) {
+		try {
+			const user = jwt.verify(token, Instance.get().settings.requestsAuth.refreshToken.key) as RefreshUser
+			if (!user) throw new NotAuthenticatedError()
+			return user
+		} catch {
+			throw new NotAuthenticatedError()
+		}
+	}
 
-	// If no cached value, means someone used your old token for a second time, so current one got deleted from cache
-	// if (!cachedRefreshToken) throw new NotAuthenticatedError()
+	async retrieveAccessTokenFor (userId: string) {
+		return Instance.get().cache.get(this.#getAccessTokenKey(userId))
+	}
 
-	// If cached value is not equal, means someone is trying to use an old token for a second time
-	// if (refreshToken !== cachedRefreshToken) {
-	// await deleteCachedAccessToken(refreshUser.id)
-	// await deleteCachedRefreshToken(refreshUser.id)
-	// throw new RefreshTokenMisusedError()
-	// }
+	async retrieveRefreshTokenFor (userId: string) {
+		return Instance.get().cache.get(this.#getRefreshTokenKey(userId))
+	}
 
-	return await makeTokens(refreshUser.id)
+	async deleteAccessTokenFor (userId: string) {
+		await Instance.get().cache.delete(this.#getAccessTokenKey(userId))
+	}
+
+	async deleteRefreshTokenFor (userId: string) {
+		await Instance.get().cache.delete(this.#getRefreshTokenKey(userId))
+	}
 }
