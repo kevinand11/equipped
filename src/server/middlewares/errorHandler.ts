fae0d16e37f50d896238f6b436710f617af12641
kevinand11 <kevinfizu@gmail.com>
2025-06-01 20:34:30 +0100

    refactor: log all errors before returning response

--- src/server/middlewares/errorHandler.ts
+++ src/server/middlewares/errorHandler.ts
@@ -3,16 +3,14 @@ import { Instance } from '../../instance'
 import { makeErrorMiddleware, StatusCodes } from '../types'
 
 export const errorHandler = makeErrorMiddleware(async (req, error) => {
-	if (error instanceof RequestError)
-		return req.error({
-			body: error.serializedErrors,
-			status: error.statusCode,
-		})
-	else {
-		Instance.get().logger.error(error)
-		return req.error({
-			body: [<any>{ message: 'Something went wrong', data: error.message }],
-			status: StatusCodes.BadRequest,
-		})
-	}
+	Instance.get().logger.error(error)
+	return error instanceof RequestError
+		? req.error({
+				body: error.serializedErrors,
+				status: error.statusCode,
+			})
+		: req.error({
+				body: [<any>{ message: 'Something went wrong', data: error.message }],
+				status: StatusCodes.BadRequest,
+			})
 })
