7800947e373315cfaaa6e56dd3d502524152157f
kevinand11 <kevinfizu@gmail.com>
2025-06-27 12:01:52 +0100

    feat: do not bundle

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+export type * from 'valleyed'
+
 export * from './overrides'
 
 export type Enum<T> = T[keyof T]
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,23 +1,16 @@
+import { fixImportsPlugin } from 'esbuild-fix-imports-plugin'
 import { defineConfig, Options } from 'tsup'
 
 const commonOptions: Options = {
-	entry: {
-		index: 'src/index.ts',
-		cache: 'src/cache/index.ts',
-		dbs: 'src/dbs/index.ts',
-		errors: 'src/errors/index.ts',
-		events: 'src/events/index.ts',
-		jobs: 'src/jobs/index.ts',
-		server: 'src/server/index.ts',
-		types: 'src/types/index.ts',
-		utilities: 'src/utilities/index.ts',
-		validations: 'src/validations/index.ts',
-	},
+	entry: ['src/**/*'],
 	sourcemap: true,
 	clean: true,
 	dts: false,
 	minify: false,
 	splitting: true,
+	bundle: false,
+	platform: 'node',
+	esbuildPlugins: [fixImportsPlugin()],
 	esbuildOptions(options) {
 		options.platform = 'node'
 	},
@@ -45,6 +38,7 @@ export default defineConfig([
 		outExtension: () => ({ js: '.cjs' }),
 	},
 	{
+		...commonOptions,
 		format: 'cjs',
 		outDir: 'dist/cjs',
 		minify: true,
@@ -52,10 +46,24 @@ export default defineConfig([
 		clean: false,
 	},
 	{
-		entry: commonOptions.entry,
-		dts: true,
+		...commonOptions,
 		format: 'esm',
 		outDir: 'dist/types',
-		clean: false,
+		sourcemap: false,
+		dts: {
+			entry: [
+				'src/index.ts',
+				'src/cache/index.ts',
+				'src/dbs/index.ts',
+				'src/errors/index.ts',
+				'src/events/index.ts',
+				'src/instance/index.ts',
+				'src/jobs/index.ts',
+				'src/server/index.ts',
+				'src/types/index.ts',
+				'src/utilities/index.ts',
+				'src/validations/index.ts',
+			],
+		},
 	},
 ])
